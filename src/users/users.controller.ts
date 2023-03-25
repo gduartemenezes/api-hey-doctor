1072b4a6f0fdc8379ed8903282908bc9890501db
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-25 10:31:18 -0300

    feat: add post route to create Admin user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { ReturnUserDto } from './dtos/return-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService){}
+
+    @Post()
+    async createAdminUser(
+        @Body() createUserDto: CreateUserDto
+    ): Promise<ReturnUserDto> {
+        const user = await this.usersService.createAdminUser(createUserDto)
+
+        return {
+            user,
+            message: 'Administrador cadastro com sucesso'
+        }
+    }
+
+
+}

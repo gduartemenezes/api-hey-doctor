394f81663c84d9649f57b39e42c697c4732b1819
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-25 10:30:06 -0300

    feat: remove users repository import

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserRepository } from './users.repository';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserRepository])],
+    imports: [],
     providers: [UsersService],
     controllers: [UsersController]
 })

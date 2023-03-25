e542660554330f8ce8af48ea2ad957fc9ce83b84
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-25 09:47:23 -0300

    feat: create user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './users.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository
+    ){}
+
+
+}

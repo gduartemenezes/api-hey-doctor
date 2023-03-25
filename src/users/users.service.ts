acdd1035385f42173a91a248b2b5ec3d95aea2a6
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-25 09:51:23 -0300

    feat: add method createAdminUser into users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserRole } from './user-roles.enum';
+import { User } from './user.entity';
 import { UserRepository } from './users.repository';
 
 @Injectable()
@@ -8,6 +11,12 @@ export class UsersService {
         @InjectRepository(UserRepository)
         private userRepository: UserRepository
     ){}
-
+    async createAdminUser(createUserDto: CreateUserDto): Promise<User> {
+        if(createUserDto.password !== createUserDto.passwordConfirmation) {
+            throw new UnprocessableEntityException('As senhas não conferem')
+        } else {
+            return this.userRepository.createUser(createUserDto, UserRole.ADMIN)
+        }
+    }
 
 }

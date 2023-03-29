392c3b190667d64d9befdede220d25d1bb4f7bd8
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-29 13:15:16 -0300

    feat: add checkPassword method to User entity

--- src/modules/users/user.entity.ts
+++ src/modules/users/user.entity.ts
@@ -1,5 +1,5 @@
 import { BaseEntity, Entity, Unique, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
-
+import * as bcrypt from 'bcrypt'
 @Entity()
 @Unique(['email'])
 export class User extends BaseEntity {
@@ -36,7 +36,10 @@ export class User extends BaseEntity {
     @UpdateDateColumn()
     updatedAt: Date;
 
-
+    async checkPassword(password: string): Promise<boolean> {
+        const hash = await bcrypt.hash(password, this.salt)
+        return hash === this.password
+    }
 
 
 

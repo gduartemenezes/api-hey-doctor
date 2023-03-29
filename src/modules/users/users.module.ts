1455576e933d49ea2cc486db0cd74f1e49212f3c
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-29 14:16:27 -0300

    feat: register passport module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,9 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
+import { PassportModule } from '@nestjs/passport';
+
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UsersRepository])],
+    imports: [
+        TypeOrmModule.forFeature([UsersRepository]),
+        PassportModule.register({defaultStrategy: 'jwt'})
+    ],
     providers: [UsersService, UsersRepository],
     controllers: [UsersController]
 })

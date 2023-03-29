12cb50f8e8c13e3b2dde3a408e20764dfbc299f8
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-29 13:15:26 -0300

    feat: import users repository

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
 
 @Module({
-    imports: [],
-    providers: [UsersService],
-    exports: [UsersService],
+    imports: [TypeOrmModule.forFeature([UsersRepository])],
+    providers: [UsersService, UsersRepository],
     controllers: [UsersController]
 })
 export class UsersModule {}
+

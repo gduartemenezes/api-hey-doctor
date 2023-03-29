2f07f135ffacceb7eabb566290c20fee7a6cba39
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-29 13:16:26 -0300

    feat: add JWT registering

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from 'src/modules/users/users.module';
+import { UsersRepository } from 'src/modules/users/users.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forFeature([UsersRepository]),
+  JwtModule.register({
+    secret: 'super-secret',
+    signOptions: {
+      expiresIn: 18000
+    }
+  }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, UsersRepository]
 })
-export class AuthModule {}
+export class AuthModule { }

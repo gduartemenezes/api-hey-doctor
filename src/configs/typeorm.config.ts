3ffbb507be170939676a32f80bc3249e41bbaf57
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-29 13:14:09 -0300

    feat: add typeOrmDataSourceOptions

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { DataSourceOptions } from "typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
@@ -10,3 +11,14 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
     entities: [__dirname + '/../**/*entity.{js,ts}'],
     synchronize: true
 };
+
+export const typeOrmDataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'pguser',
+    password: 'pgpassword',
+    database: 'nestjs',
+    entities: [__dirname + '/../**/*entity.{js,ts}'],
+    synchronize: true
+}

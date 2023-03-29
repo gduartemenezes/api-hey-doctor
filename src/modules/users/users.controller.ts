8b112a9100df8ec94b9caec57ce4d0d94c2776e0
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-29 14:16:16 -0300

    feat: implement roles guard and decorator

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/roles.guard';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { ReturnUserDto } from './dtos/return-user.dto';
+import { UserRole } from './user-roles.enum';
 import { UsersService } from './users.service';
-
+import { Role } from '../auth/role.decorator';
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService){}
 
     @Post()
+    @Role(UserRole.ADMIN)
+    @UseGuards(AuthGuard(), RolesGuard)
     async createAdminUser(
         @Body(ValidationPipe) createUserDto: CreateUserDto
     ): Promise<ReturnUserDto> {
@@ -21,3 +26,4 @@ export class UsersController {
 
 
 }
+

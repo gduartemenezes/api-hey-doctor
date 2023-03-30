b81f187deb0e70bf6788c57ce6c27812cfd62ea4
Gustavo Duarte <gduartemenezes@gmail.com>
2023-03-30 14:04:14 -0300

    feat: write method to insert new data to a user

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -9,6 +9,7 @@ import { DataSource } from 'typeorm';
 import { typeOrmConfig, typeOrmDataSourceOptions } from 'src/configs/typeorm.config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -30,6 +31,25 @@ export class UsersService {
         if(!user) throw new NotFoundException('Usuário não encontrado')
 
         return user
+    }
+
+    async updateUser(updateUserDto: UpdateUserDto, id: string): Promise<User> {
+        const user = await this.findByUserId(id)
+
+        const {name, email, role, status} = updateUserDto
+
+        user.name = name ? name : user.name
+        user.email = email ? email : user.email
+        user.role = role ? role : user.role
+        user.status = status === undefined ? user.status : status
+
+        try {
+            await user.save()
+            return user
+        } catch (error) {
+            throw new InternalServerErrorException('Erro ao salvar dados no banco de dados')
+        }
+
     }
 
 }

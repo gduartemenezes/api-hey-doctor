5c555bcc5b3618a09815e3a3f4efa8513c35f85e
Gustavo Duarte <gduartemenezes@gmail.com>
2023-04-04 10:43:32 -0300

    feat: import mailer service and use it in user creation

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,19 +6,34 @@ import { UserRole } from 'src/modules/users/user-roles.enum';
 import { User } from 'src/modules/users/user.entity';
 import { UsersRepository } from '../users/users.repository';
 import { CredentialsDto } from './dtos/credentials.dto';
+import { MailerService } from '@nestjs-modules/mailer';
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UsersRepository)
         private usersRepository: UsersRepository,
-        private jwtService: JwtService
+        private jwtService: JwtService,
+        private mailerService: MailerService
     ){}
     async singUp(createUserDto: CreateUserDto): Promise<User> {
         if(createUserDto.password !== createUserDto.passwordConfirmation) {
             throw new UnprocessableEntityException('As senhas não conferem')
         } else {
-            return await this.usersRepository.createUser(createUserDto, UserRole.USER)
+            const user = await this.usersRepository.createUser(createUserDto, UserRole.USER)
+            
+            const mail = {
+                to: user.email,
+                from: 'noreply@application.com',
+                subject:'Email de confirmação',
+                template: 'email-confirmation',
+                context: {
+                    token: user.confirmationToken
+                },
+            }
+            await this.mailerService.sendMail(mail)
+            return user
         }
+        
     }
 
     async signIn(credentialsDto: CredentialsDto) {

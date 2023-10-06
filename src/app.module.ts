d4a9819b7ff03f1e34a41007473ffbc411fb48f2
Gustavo Duarte <gduartemenezes@gmail.com>
2023-10-06 09:47:56 -0300

    feat: import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,18 +9,30 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggerInterceptor } from './interceptors/logger.interceptor';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { mailerConfig } from './configs/mailer.config';
+import { PatientModule } from './modules/patient/patient.module';
+import { DoctorWalletModule } from './modules/doctor-wallet/doctor-wallet.module';
+import { AppointmentsModule } from './modules/appointments/appointments.module';
+import { DiseasesModule } from './modules/diseases/diseases.module';
+import { Prescription } from './modules/prescriptions/prescriptions.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     WinstonModule.forRoot(winstonConfig),
     MailerModule.forRoot(mailerConfig),
     UsersModule,
-    AuthModule
+    AuthModule,
+    PatientModule,
+    DoctorWalletModule,
+    AppointmentsModule,
+    DiseasesModule,
+    Prescription,
   ],
   controllers: [],
-  providers: [{
-    provide: APP_INTERCEPTOR,
-    useClass: LoggerInterceptor
-  }],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}

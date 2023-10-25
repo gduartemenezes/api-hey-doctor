2352f3fc61269e1927f85b43aa5a994772749238
Gustavo Duarte <gduartemenezes@gmail.com>
2023-10-25 08:08:18 -0300

    chore: format files

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -1,30 +1,31 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
-import { InjectRepository } from "@nestjs/typeorm";
-import {Strategy, ExtractJwt} from 'passport-jwt'
-import { User } from "../users/user.entity";
-import { UsersRepository } from "../users/users.repository";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { User } from '../users/user.entity';
+import { UsersRepository } from '../users/users.repository';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(
-        @InjectRepository(UsersRepository)
-        private usersRepository: UsersRepository,
-    ){
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+  ) {
     super({
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-        secretOrKey: 'super-secret'
-    })}
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'super-secret',
+    });
+  }
 
-    async validate(payload:{id: string}): Promise<User> {
-        const {id} = payload
-        const user = await this.usersRepository.findOne({
-            where: {id},
-            select: ['name', 'email', 'role', 'status']
-        })
-        if(!user) {
-            throw new UnauthorizedException('Usuário não encontrado')
-        }
-
-        return user
+  async validate(payload: { id: string }): Promise<User> {
+    const { id } = payload;
+    const user = await this.usersRepository.findOne({
+      where: { id },
+      select: ['name', 'email', 'role', 'status'],
+    });
+    if (!user) {
+      throw new UnauthorizedException('Usuário não encontrado');
     }
+
+    return user;
+  }
 }
